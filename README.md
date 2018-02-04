784edad4172b0cac7fe969e62d39367df7982127
Simon van der Veldt <simon.vanderveldt@gmail.com>
2018-02-04 18:20:47 +0100

    Add custom kernel configs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Raspberry Pi 3 kernel image [![Build Status](https://travis-ci.org/simonvanderveldt/rpi3-kernel.svg?branch=master)](https://travis-ci.org/simonvanderveldt/rpi3-kernel)
+
+## Requirements
+- Docker
--- build-kernel.sh
+++ build-kernel.sh
@@ -1,31 +1,18 @@
 #!/usr/bin/env bash
-set -e
+set -x -e
 
-# # Exit if not running on Travis
-# if [ -z "$TRAVIS_BRANCH" ]; then
-#   echo "ABORTING: This script is meant for usage on Travis-CI only"
-#   exit 1
-# fi
-#
-# # Check essential envs
-# if [ -z "$GITHUB_TOKEN" ]; then
-#   echo "ABORTING: env GITHUB_TOKEN is missing"
-#   exit 1
-# fi
-
-# Show commands being run from here on
-set -x
+# Make sure we have the latest version of the image
+docker pull simonvanderveldt/rpi3-kernel-builder
 
 # run build
 docker run --rm -ti \
   -v "${PWD}/linux":/workdir/linux \
   -v "${PWD}/build":/workdir/build \
+  -v "${PWD}/configs/bcm2709stripped_defconfig":/workdir/bcm2709stripped_defconfig \
+  -v "${PWD}/patches":/workdir/patches \
+  -v "${PWD}/rt-patches":/workdir/rt-patches \
+  -v "${PWD}/monome-snd":/workdir/monome-snd \
+  -v "${PWD}/ssd1332":/workdir/ssd1332 \
+  -e DEFCONFIG_PATH=/workdir/bcm2709stripped_defconfig \
+  -e "PATCH_DIRS=/workdir/rt-patches /workdir/patches /workdir/monome-snd /workdir/ssd1332" \
   simonvanderveldt/rpi3-kernel-builder
-
-# # deploy to GitHub releases
-# export GIT_TAG=v$BUILD_NR
-# export GIT_RELTEXT="Auto-released by [Travis-CI build #$TRAVIS_BUILD_NUMBER](https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID)"
-# curl -sSL https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_linux_amd64.zip > ghr.zip
-# unzip ghr.zip
-# ./ghr --version
-# ./ghr --debug -u DieterReuter -b "$GIT_RELTEXT" $GIT_TAG builds/$BUILD_NR/
